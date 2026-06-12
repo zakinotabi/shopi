9449712686dfdda8dfcd4c07c56826ba32264e01
zakinotabi <101570337+zakno52@users.noreply.github.com>
2026-06-12 01:35:25 +0100

    hero page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,13 @@
+import styles from './Home.module.css';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles['home-intro']}>
+        <div className={styles['home-logo']}>SHOPI</div>
+        <div className={styles['home-content']}>Curated essentials for the modern lifestyle.</div>
+        <button className={styles['home-button']}>Start Shopping</button>
+      </div>
+    </div>
+  );
 }
