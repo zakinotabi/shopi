f9c5fad2f569f45fe22fea6f0960ca21c451c775
zakinotabi <101570337+zakno52@users.noreply.github.com>
2026-06-12 23:15:45 +0100

    shop page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,29 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  const activeLink = ({ isActive }) => {
+    return isActive ? `${styles.link} ${styles.active}` : styles.link;
+  };
+
   return (
     <div>
       <nav className={styles.navbar}>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/" className={activeLink}>
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/shop">Shop</Link>
+            <NavLink to="/shop" className={activeLink}>
+              Shop
+            </NavLink>
           </li>
           <li className={styles.cart}>
-            <Link to="/cart">Cart</Link>
+            <NavLink to="/cart" className={activeLink}>
+              Cart
+            </NavLink>
           </li>
         </ul>
       </nav>
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from 'react';
+import styles from './Shop.module.css';
+
 export default function Shop() {
-  return <div>Shop</div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        {items.map((item, i) => (
+          <div key={i} className={styles['item-container']}>
+            <div className={styles['item-img']}>
+              <img src={item.image} alt={item.title} />
+            </div>
+            <div className={styles['item-title']}>{item.title.split(' ').slice(0, 5).join(' ')}</div>
+            <div className={styles['item-footer']}>
+              <div className={styles['item-price']}>{item.price} $</div>
+              <button className={styles['item-btn']}>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
